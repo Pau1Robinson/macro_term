9b7e54e818e198682aaab83b959bba3f432563ee
Pau1Robinson <pr6784@gmail.com>
2019-07-04 10:44:55 +1000

    started args parse uploaded to git hub

--- macro_term.py
+++ macro_term.py
@@ -3,8 +3,12 @@ macro_term written by Paul Robinson for Assessment 1: Plan and Implement a Termi
 '''
 from os import path
 import json
+import argparse
 from pynput import keyboard
 from pynput.keyboard import Controller
+
+parser = argparse.ArgumentParser(description='.json file to import macros from')
+#TODO finish implenting argparse
 
 def options_menu():
     '''
@@ -20,14 +24,17 @@ def options_menu():
 
 def get_valid_keypress_input(input_question, invalid_input_response):
     '''
-    asks the user for input using the input-question argument and returns that input if it is valid or informs the user their input is\n
-    invalid using invalid_input_response\n
+    asks the user for input using the input-question argument\n
+    and returns that input if it is valid or informs the user their input\n
+    is invalid using invalid_input_response\n
     input_question: string outputted to the user when the input is requested\n
-    invalid_input_response: the string sent to the user when their input is invalid to inform them their input is invalid\n
+    invalid_input_response: the string sent to the user when their input is invalid\n
     '''
     user_input = input(input_question)
-    valid_keyboard_press = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', 'a', 's', 'd', 'f',
-                            'g', 'h', 'j', 'k', 'l', ';', '\'', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '*', '`']
+    valid_keyboard_press = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'q', 'w',
+                            'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', 'a', 's', 'd', 'f',
+                            'g', 'h', 'j', 'k', 'l', ';', '\'', 'z', 'x', 'c', 'v', 'b', 'n', 'm',
+                            ',', '.', '/', '*', '`']
     for key in valid_keyboard_press:
         if user_input == key:
             #print(f'your valid input is {user_input}')
@@ -37,14 +44,17 @@ def get_valid_keypress_input(input_question, invalid_input_response):
 
 def get_valid_keypress_string(string_input_question, invalid_string_response):
     '''
-    Asks the user for a string of keypresses using string_input_question and check if those keypresses are valid/n
+    Asks the user for a string of keypresses using string_input_question/n
+    and check if those keypresses are valid/n
     string_input_question: String outputted to the user when the input is requested/n
-    invalid_string_response: The string sent to the user when the string they inputted is invalid to inform them their string is invalid\n
+    invalid_string_response: The string sent to the user when the string they inputted is invalid\n
     '''
     #NTS add support for capitalization? change to using pynput instead of input?
     user_input = input(string_input_question)
-    valid_keyboard_press = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', 'a', 's', 'd', 'f',
-                            'g', 'h', 'j', 'k', 'l', ';', '\'', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '*', '`']
+    valid_keyboard_press = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'q', 'w',
+                            'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', 'a', 's', 'd', 'f',
+                            'g', 'h', 'j', 'k', 'l', ';', '\'', 'z', 'x', 'c', 'v', 'b', 'n', 'm',
+                            ',', '.', '/', '*', '`']
     user_input_list = [char for char in user_input]
     is_valid = False
     for char in user_input_list:
@@ -58,8 +68,9 @@ def get_valid_keypress_string(string_input_question, invalid_string_response):
 
 def get_valid_filename(filename_input_question, invalid_filename_response):
     '''
-    Asks the user to input a filename using filename_input_question and check if they inputted a valid filename/n
-    either returning the filename if its valid or printing invalid_filename_response if its not/n
+    Asks the user to input a filename using filename_input_question
+    and check if they inputted a valid filename either returning the filename if its valid/n
+    or printing invalid_filename_response if its not/n
     filename_input_question: The string printed to the user when asking for the filename/n
     invalid_filename_response: The string printed to the user when their filename is valid
     '''
@@ -73,18 +84,17 @@ def get_valid_filename(filename_input_question, invalid_filename_response):
                     is_valid = False
         if is_valid is True:
             return user_input
-    else: 
-        print(invalid_filename_response)
-        return 'fail'
+    print(invalid_filename_response)
+    return 'fail'
 
-def get_valid_file_path(path_input_question, invalid_path_response):
+def get_valid_file_path(path_input, invalid_path_response):
     '''
     Requests input from the user for a file path then checks if the path exists\n
     and returns it if does or outputs invalid_path_response if it doesn't\n
-    path_input_question: The string to be outputted to the user when asking for the file path\n
+    path_input: The string to be outputted to the user when asking for the file path\n
     invalid_path_response: The string to be outputted to the user if the file path is invalid
     '''
-    path_input = input(path_input_question)#NTS add support for filename without path?
+#NTS add support for filename without path?
     if path.exists(path_input):
         return path_input
     print(invalid_path_response)
@@ -93,7 +103,7 @@ def get_valid_file_path(path_input_question, invalid_path_response):
 def save_macro(save_dict, filename):
     '''
     Saves save_dict to a .json file using filename as the name of the file
-    save_dict: the dictionary to be saved to the file 
+    save_dict: the dictionary to be saved to the file
     filename: the name to be used for the file
     '''
     if filename != 'fail':
@@ -118,10 +128,12 @@ def import_macro(file_path, import_dict):
         data_dict = json.loads(data)
         import_dict.update(data_dict)
         return import_dict
+    return import_dict
 
 def create_macro(keypress_input, keypress_string, create_dict):
     '''
-    Takes input from keypress_input and keypress_string and writes it into create_dict to store the macro/n
+    Takes input from keypress_input and keypress_string and writes it into create_dict/n
+    to store the macro/n
     keypress_input: The first keypress that will be used to activate the macro/n
     Keypress_string: The keypresses that will be outputted by the macro/n
     create_dict: The dictionary where keypress_input and keypress_string are added to
@@ -145,7 +157,8 @@ def listening_mode(listen_dict):
     currently_pressed_macro = set()
     def on_press(key):
         '''
-        uses currently_pressed to keep track of the keys being presses, then check if those keys are a macro shortcut/n
+        uses currently_pressed to keep track of the keys being presses,/n
+        then check if those keys are a macro shortcut/n
         and passes them to currently_pressed_macro if they are
         '''
         print(f'{key} pressed')#NTS remove after debuging
@@ -160,11 +173,10 @@ def listening_mode(listen_dict):
                     print(f'macro {key} triggered')#NTS remove after debuging
                     currently_pressed_macro.add(f'macro is pressed')
                     currently_pressed_macro.add(macro)
-    
     def on_release(key):
         '''
-        uses currently_pressed_macro to check if a macro shortcut is pressed and on the release of that shortcut activates the macro/n
-        using output_macro
+        uses currently_pressed_macro to check if a macro shortcut is pressed\n
+        and on the release of that shortcut activates the macro using output_macro
         '''
         try:
             currently_pressed.remove(key)
@@ -181,10 +193,10 @@ def listening_mode(listen_dict):
         if key == keyboard.Key.esc:
             print('esc pressed')#NTS remove after debuging
             listener.stop()
-    
     def output_macro(activated_macro, output_dict):
         '''
-        checks output_dict for the key activated_macro and then outputs the value of activated_macro using pynput keyboard controller
+        checks output_dict for the key activated_macro\n
+        then outputs the value of activated_macro using pynput keyboard controller
         '''
         print('output trigged')#NTS remove after debuging
         print(output_dict)#NTS remove after debuging
@@ -199,7 +211,6 @@ def listening_mode(listen_dict):
                 keyboard.release(char)
         else:
             print('not found')
-    
     with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
         listener.join()
 
@@ -209,20 +220,26 @@ def main():
     '''
     macro_dict = {}
     options_menu()
-    create_string = 'your macro will be activated by holding alt and a key of your choice. please type the key you would like to use\n'
+    create_string = 'your macro will be activated by holding alt and a key of your choice'
+    create_string2 = 'please type the key you would like to use\n'
     main_input = input()
     while main_input != '7':
         if main_input == '1':
-            macro_dict = create_macro(get_valid_keypress_input(create_string, 'that is not a valid key'),
-                                      get_valid_keypress_string('enter the keystrokes you would like your macro to output\n',
-                                      'those are not valid keystokes'), macro_dict)
+            macro_dict = create_macro(
+                get_valid_keypress_input(create_string + create_string2, 'that is not a valid key'),
+                get_valid_keypress_string(
+                    'enter the keystrokes you would like your macro to output\n',
+                    'those are not valid keystokes'), macro_dict)
             print(macro_dict)
             main_input = input()
         elif main_input == '4':
-            macro_dict = import_macro(get_valid_file_path('Enter the path of the file you want to input\n', 'That is not a valid file path'), macro_dict)
+            path_input = input('Enter the path of the file you want to input\n')
+            macro_dict = import_macro(get_valid_file_path(path_input,
+                'That is not a valid file path'), macro_dict)
             main_input = input()
         elif main_input == '5':
-            save_macro(macro_dict, get_valid_filename('What would you like your file to be named?\n', 'That is not a valid filename'))
+            save_macro(macro_dict, get_valid_filename('What would you like your file to be named?\n'
+                                                        , 'That is not a valid filename'))
             main_input = input()
         elif main_input == '6':
             listening_mode(macro_dict)
